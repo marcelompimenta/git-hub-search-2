61fb93909d1ebaa17b646da3a0d799f4072fc113
marcelo pimenta <marcelopimenta.work@gmail.com>
2022-06-26 02:48:44 -0400

    I created the base styles of css in the project, I made the function that and makes the requests to the api of the git hub, and I already captured the preliminary information.

--- src/ProfileComponent/ProfileComponent.jsx
+++ src/ProfileComponent/ProfileComponent.jsx
@@ -1,15 +1,19 @@
-function Profile(data) {
+import './ProfileComponent.css'
+function Profile({ data }) {
+    const { avatar_url, name, html_url } = data
     return (
         <>
-            <div>
-                <div>
-                    <img src="" alt="" />
+            <div className='container'>
+                <div className='container-image'>
+                    <img
+                        src={avatar_url}
+                        alt="fotinho" />
                 </div>
-                <div>
-                    <span>Nome Do Individuo</span>
+                <div className='name'>
+                    <span>{name}</span>
                 </div>
-                <div>
-                    <a href="">Link do Perfil</a>
+                <div className='link-profile'>
+                    <a target={'_blank'} href={html_url}>Link do Perfil</a>
                 </div>
             </div>
         </>
