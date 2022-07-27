2cddf091e5cd4b7e21d9deba4e9b434231425cef
marcelo pimenta <marcelopimenta.work@gmail.com>
2022-07-26 22:00:02 -0400

    New modifications

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 import IdGenerate from "./Hooks/IdGenerate"
 import Repositories from "./RepositoriesComponent/RepositoriesComponent"
 import Modal from "./ModalComponent/Modal"
-import getIndexObject from "./Hooks/getIndexObject"
+import getIndexObject from "./Hooks/getIndexObject" // não está em uso
 
 function App() {
 
@@ -16,6 +16,7 @@ function App() {
 
 
   const url = 'https://api.github.com/users/marcelompimenta'
+  // passar o usuario chapado, por enquanto!
 
   useEffect(() => {
     axios.get(url)
@@ -37,7 +38,7 @@ function App() {
   }
 
   function modifyVisibillity() {
-    if (!isVisible) setIsVisible(true);
+    if (!isVisible) setIsVisible(true); // funcão que seta o estado do modal
   }
 
   return (
